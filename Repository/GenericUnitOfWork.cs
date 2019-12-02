4b50e514b252179b42d40ac4dae96569a8c2c48a
Sifat12 <38453547+Sifat12@users.noreply.github.com>
2019-12-02 18:44:50 +0600

    Repository

---
+++ Repository/GenericUnitOfWork.cs
@@ -0,0 +1,41 @@
+﻿using KeenaKaata.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KeenaKaata.Repository
+{
+    public class GenericUnitOfWork:IDisposable
+    {
+        private dbKeenaKaataOnlineShoppingEntities DBEntity = new dbKeenaKaataOnlineShoppingEntities();
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    DBEntity.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+        private bool disposed = false;
+
+        public IRepository<Tbl_EntityType> GetRepositoryInstance<Tbl_EntityType>() where Tbl_EntityType : class
+        {
+            return new GenericRepository<Tbl_EntityType>(DBEntity);
+        }
+        public void SaveChanges()
+        {
+            DBEntity.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
